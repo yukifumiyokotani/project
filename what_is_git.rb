668d96ab0e1bdce13b69a595ec5567f41d0c1567
yukifumi_yokotani <yokoani2gitmail.com>
2014-05-29 23:19:54 +0900

    helpメソッドを追加

--- what_is_git.rb
+++ what_is_git.rb
@@ -1,5 +1,10 @@
 Class WhatIsGit
-	def show
-		put 'Git is easy if you understand the basis'
-	end
+  def show
+    put 'Git is easy if you understand the basis'
+  end
+
+  def help
+    # コードは未定
+  end
+
 end
