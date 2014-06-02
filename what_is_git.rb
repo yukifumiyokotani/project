c0c5df84ea6b0dc9ee6f17685422b8e19c322eb9
Bob <bob@example.com>
2014-06-02 17:42:21 +0900

    aboutメソッドを多言語対応にする

--- what_is_git.rb
+++ what_is_git.rb
@@ -1,14 +1,10 @@
 Class WhatIsGit
-  def about
-    puts 'http://en.wikipedia.org/wiki/Git'
+  def about(lang = 'en')
+    puts 'http://#{lang}.wikipedia.org/wiki/Git'
   end
   
   def show
     put 'Git is easy if you understand the basis'
-  end
-
-  def help
-    # 競合してしまった 
   end
 
 end
